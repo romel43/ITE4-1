e4c866e72bb5fd1c8ec9822bb098ce5c6547944d
Batonghinog1 <romelbuyo43@gmail.com>
2023-01-11 20:43:07 +0800

    buyo

---
+++ BUYO-ITE4-1-Activity/store/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.store, name='store'),
+    path('services/', views.services, name='services'),
+    path('about/', views.about, name='about'),
+    path('plants/', views.plants, name='plants'),
+    path('seeds/', views.seeds, name='seeds'),
+    path('cuttings/', views.cuttings, name='cuttings'),
+    path('fruits/', views.fruits, name='fruits'),
+    path('flowers/', views.flowers, name='flowers'),
+    path('vegetables/', views.vegetables, name='vegetables'),
+    path('medicine/', views.medicine, name='medicine'),
+    path('cart/', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('update_item/', views.updateItem, name='update_item'),
+    path('process_order/', views.processOrder, name='process_order'),
+]
